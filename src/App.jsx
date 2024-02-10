9d48642af9fb408165e49f0dd2ad60a6797122dc
Akshay sawant <134278084+Akshaysawant917@users.noreply.github.com>
2024-02-10 23:59:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
 // src/App.jsx
 import React, { useState} from "react";
 import { useTransition, animated } from "react-spring";
+import TextEditor from "./TextEditor";
+import Form from "./Form";
+import Data from "./Data";
 
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import ClearIcon from "@mui/icons-material/Clear";
-import { Button,Box, Container, Stack} from "@mui/material";
-import zIndex from "@mui/material/styles/zIndex";
+import { Button, Box, Container, Stack} from "@mui/material";
+
+
 function App() {
   const [count, setCount] = useState(0);
   const [boxes, setBoxes] = useState([]);
@@ -42,35 +45,64 @@ function App() {
     enter: { opacity: 1, transform: "translate3d(0,0,0)" },
     leave: { opacity: 0, transform: "translate3d(0,-40px,0)" }
   });
+
+
+  const [formData, setFormData] = useState({
+    address: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
   
-    <div>
-      <Container maxWidth="xl" style={{ display:"flex",flexDirection:"column",width:"100%" ,height:"100vh", alignItems:"center",padding:"0px",backgroundColor:"black"}}>
-        <Box style={{position:"absolute",top:"30vh",zIndex:"5",display:"flex",flexDirection:"column" ,justifyContent:"center", alignItems:"center",padding:"0px",color:"yellow"}}>
+   
+      <Container maxWidth="xl" style={{ display:"flex",flexWrap:"wrap",width:"100vw" ,height:"auto",padding:"0px",backgroundColor:"black",margin:"0px"}}>
+        <Box style={{height:"50vh",width:"50%",zIndex:"5",display:"flex",flexDirection:"column",alignItems:"center",justifyContent: "center",padding:"0px",color:"yellow",border:"1px solid white"}}>
         <Box><h1>Count is {count}</h1></Box>
        <Box style={{display:"flex"}}> 
-        <Button variant="contained" color="primary" size="small" style={{ margin: "2px" }}  endIcon={<RemoveIcon />} onClick={()=>{
-          DecreaseCount()
-        }}>
+        <Button variant="contained" color="primary" size="small" style={{ margin: "2px" }}  endIcon={<RemoveIcon />} onClick={
+          DecreaseCount
+        }>
           Decrease
         </Button>
-        <Button variant="contained" color="primary"size="small" style={{ margin: "2px" }} endIcon={<ClearIcon />}onClick={()=>{
-          ResetCount()
-        }}>
+        <Button variant="contained" color="primary"size="small" style={{ margin: "2px" }} endIcon={<ClearIcon />}onClick={
+          ResetCount
+        }>
           Reset
         </Button>
-        <Button variant="contained" color="primary"size="small" style={{ margin: "2px" }} endIcon={<AddIcon />} onClick={()=>{
-          IncreaseCount()
-        }}>
+        <Button variant="contained" color="primary"size="small" style={{ margin: "2px" }} endIcon={<AddIcon />} onClick={
+          IncreaseCount
+        }>
           Increase
         </Button> 
         </Box>
         </Box>
+
         
-        <Stack style={{alignItems:"center", flexDirection:"column-reverse",height:"100%",width:"100%",}}>
+        <TextEditor/>
+        <Data formData={formData}/>
+        <Form handleChange={handleChange} handleSubmit={handleSubmit}formData={formData}/>
+        
+      
+        
+        <Stack style={{position:"absolute",alignItems:"center", flexDirection:"column-reverse",height:"100%",width:"100%",}}>
         {transitions((style, item) =>
           item && (
-            <animated.div style={{ ...style}}>
+            <animated.div key={item.id} style={{ ...style}}>
               <Box style={{ backgroundColor: "white", width: "85vw", height: "30px", margin: "5px", borderRadius: 8 }}></Box>
             </animated.div>
           )
@@ -78,7 +110,7 @@ function App() {
      </Stack>
       </Container>
      
-    </div>
+
   );
 }
 
